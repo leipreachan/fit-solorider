1dc5a03b65c9b836435a7643164067be5eed372f
Artem <2929348+leipreachan@users.noreply.github.com>
2024-01-10 12:57:11 +0000

    reformating Highcharts.svelte

--- src/components/Highcharts.svelte
+++ src/components/Highcharts.svelte
@@ -5,7 +5,7 @@
 	import { writable } from 'svelte/store';
 	import Table from './Table.svelte';
 
-	const sourceName = 'Source';
+	const sourceNameParam = 'Source';
 
 	export let metricsData = [];
 	const newSeries = new Map();
@@ -20,9 +20,23 @@
 		title: {
 			text: ''
 		},
-		colors: [ "#fe6a35", "#6b8abc", "#d568fb", "#2ee0ca", "#fa4b42", "#feb56a", "#91e8e1", "#2caffe", "#544fc5", "#00e272" ],
+		colors: [
+			'#fe6a35',
+			'#6b8abc',
+			'#d568fb',
+			'#2ee0ca',
+			'#fa4b42',
+			'#feb56a',
+			'#91e8e1',
+			'#2caffe',
+			'#544fc5',
+			'#00e272'
+		],
 		chart: {
 			zoomType: 'x'
+		},
+		tooltip: {
+			valueDecimals: 0
 		},
 		plotOptions: {
 			series: {
@@ -44,8 +58,20 @@
 	let charts = new Map();
 	let seriesNames = new Set();
 
-	const priority = new Map([['power', ['watts']], ['cadence', ['rpm']], ['heart_rate', ['bpm']], ['altitude', ['meters', 'area']], ['temperature', ['degrees']]]);
-	const exclude = new Set(['timestamp', 'position_lat', 'position_long', 'elapsed_time', 'timer_time']);
+	const priority = new Map([
+		['power', ['watts']],
+		['cadence', ['rpm']],
+		['heart_rate', ['bpm']],
+		['altitude', ['meters', 'area']],
+		['temperature', ['degrees']]
+	]);
+	const exclude = new Set([
+		'timestamp',
+		'position_lat',
+		'position_long',
+		'elapsed_time',
+		'timer_time'
+	]);
 
 	const initChart = (metricName) => {
 		if (!charts.has(metricName)) {
@@ -76,10 +102,11 @@
 		}
 
 		// Step 2: Raise the resulting values to the fourth power
-		const fourthPowerValues = rollingAverages.map(value => Math.pow(value, 4));
+		const fourthPowerValues = rollingAverages.map((value) => Math.pow(value, 4));
 
 		// Step 3: Determine the average of these values
-		const averageFourthPower = fourthPowerValues.reduce((sum, value) => sum + value, 0) / fourthPowerValues.length;
+		const averageFourthPower =
+			fourthPowerValues.reduce((sum, value) => sum + value, 0) / fourthPowerValues.length;
 
 		// Step 4: Find the fourth root of the resulting average
 		return Math.round(Math.pow(averageFourthPower, 1 / 4));
@@ -94,23 +121,27 @@
 	};
 
 	const percDiff = (a, b) => {
-		return ' Δ' + Math.round((b - a) * 100 / a) + '%';
+		return ' Δ' + Math.round(((b - a) * 100) / a) + '%';
 	};
 
 	const addPowerData = (name, data) => {
 		addAvgMaxData(name, 'power', data, {
-			'Average': calculateAverage,
-			'Normalised': calculateNormalizedPower,
-			'Max': calculateMax
-		})
+			Average: calculateAverage,
+			Normalised: calculateNormalizedPower,
+			Max: calculateMax
+		});
 	};
 
-	const addAvgMaxData = (sourceName, metricName, data, fields = {
-		'Average': calculateAverage,
-		'Max': calculateMax
-	}) => {
-
-		const newValues = {[sourceName]: sourceName};
+	const addAvgMaxData = (
+		sourceName,
+		metricName,
+		data,
+		fields = {
+			Average: calculateAverage,
+			Max: calculateMax
+		}
+	) => {
+		const newValues = { [sourceNameParam]: sourceName };
 		const m = $moreData[metricName] || [];
 		for (let [k, cb] of Object.entries(fields)) {
 			let val = cb(data);
@@ -147,9 +178,7 @@
 	};
 
 	const getMetricNames = async (data) => {
-		metricNames = new Set(
-			data.flatMap((fit) => fit.data.flatMap((x) => Object.keys(x)))
-		);
+		metricNames = new Set(data.flatMap((fit) => fit.data.flatMap((x) => Object.keys(x))));
 
 		// console.log(metricNames);
 	};
@@ -157,12 +186,14 @@
 	afterUpdate(() => {
 		// Update chart series when data changes
 		if (metricsData.length > 0) {
-
 			const fields = [...priority.keys()];
 			for (let i = 0; i < fields.length; i++) {
-				const values = metricsData.filter((m) => !seriesNames.has(m.name)).map(
-					(fit) =>
-						({ name: fit.name, data: fit.data.map((x) => [Date.parse(x.timestamp), x[fields[i]] || null]) }));
+				const values = metricsData
+					.filter((m) => !seriesNames.has(m.name))
+					.map((fit) => ({
+						name: fit.name,
+						data: fit.data.map((x) => [Date.parse(x.timestamp), x[fields[i]] || null])
+					}));
 				newSeries.set(fields[i], values);
 			}
 
@@ -188,41 +219,40 @@
 			chrt.redraw(true);
 		}
 	}
-
 </script>
-
-<style>
-    .chart_wrapper {
-        margin-bottom: 3em;
-    }
-
-    .range {
-        font-family: Helvetica, serif;
-        text-align: center;
-    }
-
-    .range [type=range] {
-        width: 800px;
-    }
-
-</style>
 
 <div id="container_wrapper">
 	{#each priority.keys() as key}
 		<div class="chart_wrapper">
-			<div id={"chartContainer_" + key}></div>
+			<div id={'chartContainer_' + key}></div>
 			{#if $moreData[key]?.length > 0}
 				<Table tableData={$moreData[key]} style={blueStyle} />
 			{:else}
 				<center>No {key} data found in the uploaded files</center>
 			{/if}
 		</div>
-		{#if key === "power" && $moreData[key]?.length > 1}
-				<div class="range">
-					Shift second chart by:<br>
-					<input type="range" min="-120" max="120" bind:value on:change={handleOnRangeChange}><br>{value}
-					seconds
-				</div>
+		{#if key === 'power' && $moreData[key]?.length > 1}
+			<div class="range">
+				Shift second chart by:<br />
+				<input type="range" min="-120" max="120" bind:value on:change={handleOnRangeChange} /><br
+				/>{value}
+				seconds
+			</div>
 		{/if}
 	{/each}
 </div>
+
+<style>
+	.chart_wrapper {
+		margin-bottom: 3em;
+	}
+
+	.range {
+		font-family: Helvetica, serif;
+		text-align: center;
+	}
+
+	.range [type='range'] {
+		width: 800px;
+	}
+</style>
