9bba6b65c04eee83dbc1ef170e801ab1edc0fb6e
Artem <2929348+leipreachan@users.noreply.github.com>
2024-01-13 17:48:50 +0000

    better table style

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,72 +1,35 @@
 <script>
+      import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
+
 	export let tableData = [];
 	export let style;
 </script>
 
-<div class="tableWraper">
-<table class={style}>
-	<thead>
-	<tr>
-		{#each Object.keys(tableData[0]) as columnHeading}
-			<th>{columnHeading}</th>
-		{/each}
-	<tr/>
-	</thead>
-	<tbody>
+<div class="table-wrapper">
+<Table hoverable={true}>
+	<TableHead>
+    {#each Object.keys(tableData[0]) as columnHeading}
+        <TableHeadCell>{columnHeading}</TableHeadCell>
+    {/each}
+	</TableHead>
+	<TableBody class="divide-y">
 	{#each Object.values(tableData) as row}
-		<tr>
+		<TableBodyRow>
 			{#each Object.values(row) as cell}
-				<td>{cell}</td>
+				<TableBodyCell>{cell}</TableBodyCell>
 			{/each}
-		</tr>
+		</TableBodyRow>
 	{/each}
-	</tbody>
-</table>
+	</TableBody>
+</Table>
 </div>
 
 <style>
-    table, th, td {
-        border: 1px solid;
-        border-collapse: collapse;
-        margin-bottom: 10px;
-        font-family: Helvetica,serif;
+    .table-wrapper {
+        @apply flex justify-center;
     }
 
-    tbody tr td:first-child {
-        width: 25em;
-        max-width: 30em;
-        word-break: break-all;
-    }
-
-    table.blueTable {
-        margin: 0 auto;
-        border: 1px solid #a3c1d5;
-        background-color: #EEEEEE;
-        border-collapse: collapse;
-        text-align: center;
-				margin-bottom: 3em;
-    }
-    table.blueTable td, table.blueTable th {
-        border: 1px solid #AAAAAA;
-        padding: 3px 2px;
-    }
-    table.blueTable tbody td {
-        font-size: 13px;
-    }
-    table.blueTable tr:nth-child(even) {
-        background: #D0E4F5;
-    }
-    table.blueTable thead {
-        background: #a3c1d5;
-        border-bottom: 2px solid #c0c0c0;
-    }
-    table.blueTable thead th {
-        font-size: 15px;
-        font-weight: bold;
-        color: #FFFFFF;
-        border-left: 2px solid #D0E4F5;
-    }
-    table.blueTable thead th:first-child {
-        border-left: none;
+    table {
+        @apply w-3/4 border-collapse;
     }
 </style>
