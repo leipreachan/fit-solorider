c7d74595ef61d3691fb3f84efe587f2318290d72
Artem <2929348+leipreachan@users.noreply.github.com>
2024-01-30 23:55:15 +0000

    added more tranlsations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,11 @@
 
 	register('en', () => import('$lib/i18n/locales/en.json'));
 	register('en-US', () => import('$lib/i18n/locales/en.json'));
+	register('en-GB', () => import('$lib/i18n/locales/en.json'));
+	register('es', () => import('$lib/i18n/locales/es.json'));
+	register('fr', () => import('$lib/i18n/locales/fr.json'));
+	register('de', () => import('$lib/i18n/locales/de.json'));
+	register('ua', () => import('$lib/i18n/locales/ua.json'));
 	register('ru', () => import('$lib/i18n/locales/ru.json'));
 
 	const setup = async () => {
