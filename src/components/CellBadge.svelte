fdf5bf63212e4f9a76dcca48048f69671e3b02d9
Artem <2929348+leipreachan@users.noreply.github.com>
2024-01-31 07:24:41 +0000

    forgot to commit bunch of files :facepalm:

---
+++ src/components/CellBadge.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Badge } from 'flowbite-svelte';
+	export let diff: number;
+</script>
+
+{#if diff != 0}
+	<Badge color={diff > 0 ? 'red' : 'blue'} title={'compared to the first row'}>{diff}%</Badge>
+{/if}
