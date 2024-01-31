0754ca8877e8ac906a24e2896c3c4292330e4596
Artem <2929348+leipreachan@users.noreply.github.com>
2024-01-31 08:39:20 +0000

    more languages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,13 @@
 	register('de', () => import('$lib/i18n/locales/de.json'));
 	register('ua', () => import('$lib/i18n/locales/ua.json'));
 	register('ru', () => import('$lib/i18n/locales/ru.json'));
+	register('it', () => import('$lib/i18n/locales/it.json'));
+	register('pt', () => import('$lib/i18n/locales/pt.json'));
+	register('pl', () => import('$lib/i18n/locales/pl.json'));
+	register('ar', () => import('$lib/i18n/locales/ar.json'));
+	register('zh', () => import('$lib/i18n/locales/zh.json'));
+	register('ko', () => import('$lib/i18n/locales/ko.json'));
+	register('jp', () => import('$lib/i18n/locales/jp.json'));
 
 	const setup = async () => {
 		await init({
