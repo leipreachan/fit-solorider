c4850c6c32ef1a868ebd3dbf8147687dc30ff7fc
Artem <2929348+leipreachan@users.noreply.github.com>
2024-02-10 21:30:51 +0000

    autodetect color theme of the device

--- src/stores/theme.js
+++ src/stores/theme.js
@@ -1,10 +1,9 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-const defaultValue = 'light';
+const defaultValue = browser ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light') : '';
 const initial = browser ? window.localStorage?.theme ?? defaultValue : defaultValue;
-
-export const theme = writable(initial || 'light');
+export const theme = writable(initial || defaultValue);
 
 theme.subscribe((value) => {
     if (browser) {
